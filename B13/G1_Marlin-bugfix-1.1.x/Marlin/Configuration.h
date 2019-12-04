5eef6030db4ccdb932f9b52d46300a4464dcee24
yah1998 <50864217+yah1998@users.noreply.github.com>
2019-12-04 23:20:58 +0800

    upload

--- B13/G1_Marlin-bugfix-1.1.x/Marlin/Configuration.h
+++ B13/G1_Marlin-bugfix-1.1.x/Marlin/Configuration.h
@@ -1074,7 +1074,7 @@
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  * By default the firmware assumes HIGH=FILAMENT PRESENT.
  */
-//#define FILAMENT_RUNOUT_SENSOR
+#define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define NUM_RUNOUT_SENSORS   1     // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
   #define FIL_RUNOUT_INVERTING false // set to true to invert the logic of the sensor.
@@ -1427,7 +1427,7 @@
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
   // #define NOZZLE_PARK_POINT { (X_MIN_POS + 10), 0, 20 }
-  #define NOZZLE_PARK_POINT { 0, 0, Z_MAX_POS-10}
+  #define NOZZLE_PARK_POINT { 0, 0, (Z_MAX_POS-100)}
   #define NOZZLE_PARK_XY_FEEDRATE 100   // X and Y axes feedrate in mm/s (also used for delta printers Z axis)
   //#define NOZZLE_PARK_Z_FEEDRATE 5      // Z axis feedrate in mm/s (not used for delta printers)
 #endif
--- B13/G1_Marlin-bugfix-1.1.x/Marlin/Configuration_adv.h
+++ B13/G1_Marlin-bugfix-1.1.x/Marlin/Configuration_adv.h
@@ -1039,7 +1039,7 @@
  * Requires NOZZLE_PARK_FEATURE.
  * This feature is required for the default FILAMENT_RUNOUT_SCRIPT.
  */
-//#define ADVANCED_PAUSE_FEATURE //斷料
+#define ADVANCED_PAUSE_FEATURE // 暫停   斷料
 #if ENABLED(ADVANCED_PAUSE_FEATURE)
   #define PAUSE_PARK_RETRACT_FEEDRATE         60  // (mm/s) Initial retract feedrate.
   #define PAUSE_PARK_RETRACT_LENGTH            2  // (mm) Initial retract.
